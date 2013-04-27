2885275765eecf02040db97b1aa14d62e844ccf3
Marc Neuhaus <mneuhaus@famelo.com>
2013-04-28 00:32:18 +0200

    Cleanup

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,7 +6,7 @@
  * This is used e.g. to add a custom <html>-tag, to modify the <meta charset>-tag
  * or to add the X-UA-Compatible parameter directly after the <meta charset>-tag
  */
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postProcess'][] = 'EXT:' . $_EXTKEY . '/Classes/class.tx_html5boilerplate.php:tx_html5boilerplate->pageRendererPostProcessHook';
+// $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postProcess'][] = 'EXT:' . $_EXTKEY . '/Classes/class.tx_html5boilerplate.php:tx_html5boilerplate->pageRendererPostProcessHook';
 
 // $TYPO3_CONF_VARS['EXT']['news']['templateLayouts']['myext'] = array('Home Teaser', 'home-teaser');
 ?>
